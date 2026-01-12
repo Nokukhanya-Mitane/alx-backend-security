dab78082f36c54eba4679aa5fa8582aa68bd6789
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-12 10:05:11 +0200

    Update middleware.py

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,28 +1,28 @@
-from django.utils import timezone
-from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from ipgeolocation import IpGeolocationAPI
 from ip_tracking.models import RequestLog, BlockedIP
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.geo = IpGeolocationAPI()
 
     def __call__(self, request):
         ip = self.get_client_ip(request)
 
-        # Block blacklisted IPs (Task 1)
         if BlockedIP.objects.filter(ip_address=ip).exists():
             return HttpResponseForbidden("Forbidden")
+
+        geo_data = cache.get(ip)
+        if not geo_data:
+            geo_data = self.geo.get_geolocation(ip)
+            cache.set(ip, geo_data, 60 * 60 * 24)
 
         RequestLog.objects.create(
             ip_address=ip,
             path=request.path,
-            timestamp=timezone.now()
+            country=geo_data.get('country_name'),
+            city=geo_data.get('city')
         )
 
         return self.get_response(request)
-
-    def get_client_ip(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            return x_forwarded_for.split(',')[0]
-        return request.META.get('REMOTE_ADDR')
