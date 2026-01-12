3913455f7daea831965b35650147c0b6701e394b
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-12 09:58:21 +0200

    Create models.py

---
+++ ip_tracking/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Used later (Tasks 2 & 4)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path}"
